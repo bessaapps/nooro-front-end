2537c84619f0045e430863d62d91817325e981cd
topher <topherjamesknoll@gmail.com>
2025-08-24 22:14:02 -0700

    Think twice. Code once.

--- app/tasks/[taskId]/page.js
+++ app/tasks/[taskId]/page.js
@@ -1,10 +1,15 @@
 import EditForm from "@/components/EditForm";
+import { Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default async function EditTaskPage({ params }) {
   const { taskId } = await params;
 
   return (
     <>
+      <Link href={"../"}>
+        <Text mb={4}>Go Back</Text>
+      </Link>
       <EditForm taskId={taskId} />
     </>
   );
--- components/Tasks.js
+++ components/Tasks.js
@@ -1,6 +1,20 @@
 "use client";
 
-import { Badge, Box, Flex, Progress, Stack, Text } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Progress,
+  Stack,
+  Text
+} from "@chakra-ui/react";
 import { useTask } from "@/context/TasksContext";
 import Checkbox from "@/assets/images/checkbox.svg";
 import CompletedCheckbox from "@/assets/images/completed-checkbox.svg";
@@ -8,8 +22,12 @@ import Trash from "@/assets/images/icons/trash.svg";
 import Image from "next/image";
 import { deleteTaskAPI, updateTaskAPI } from "@/utils/api";
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 export default function Tasks() {
+  const [isConfirming, setIsConfirming] = useState(false);
+  const [activeTaskId, setActiveTaskId] = useState(undefined);
+  const cancelRef = useRef();
   const { tasks, setTasks, isFetchingTasks, setIsFetchingTasks } = useTask();
 
   const toggleTask = (id, completed) => {
@@ -32,88 +50,120 @@ export default function Tasks() {
       .finally(() => setIsFetchingTasks(false));
   };
 
-  const destroyTask = (id) => {
-    if (!id) return;
+  const destroyTask = () => {
+    if (!activeTaskId) return;
     setIsFetchingTasks(true);
 
-    deleteTaskAPI(id)
-      .then(() => setTasks((prev) => prev?.filter((item) => item?.id !== id)))
+    deleteTaskAPI(activeTaskId)
+      .then(() =>
+        setTasks((prev) => prev?.filter((item) => item?.id !== activeTaskId))
+      )
       .catch((error) => console.error(error))
-      .finally(() => setIsFetchingTasks(false));
+      .finally(() => {
+        setIsConfirming(false);
+        setIsFetchingTasks(false);
+      });
   };
 
   return (
-    <Stack>
-      <Flex justify={"space-between"} mb={2}>
-        <Flex gap={4}>
-          <Text color={"primary.400"} fontSize={14} fontWeight={700}>
-            Tasks
-          </Text>
-          <Badge borderRadius={"full"} py={"2px"} px={2}>
-            {tasks?.length}
-          </Badge>
-        </Flex>
-        <Flex gap={4}>
-          <Text color={"secondary.400"} fontSize={14} fontWeight={700}>
-            Completed
-          </Text>
-          <Badge
-            borderRadius={"full"}
-            py={"2px"}
-            px={2}
-            style={{ textTransform: "lowercase" }}
-          >
-            {tasks?.filter((task) => task?.completed)?.length} of{" "}
-            {tasks?.length}
-          </Badge>
-        </Flex>
-      </Flex>
-      {isFetchingTasks ? (
-        <Progress size="xs" isIndeterminate />
-      ) : (
-        <Box h={"4px"} />
-      )}
-      {tasks?.map(({ id, title, color, completed }) => (
-        <Flex
-          key={id}
-          align={"center"}
-          gap={4}
-          bg={"gray.800"}
-          w={"100%"}
-          borderWidth={"1px"}
-          borderColor={"gray.700"}
-          borderRadius={8}
-          p={4}
-        >
-          <Box
-            minW={"18px"}
-            cursor={"pointer"}
-            onClick={() => toggleTask(id, !completed)}
-          >
-            <Image
-              src={completed ? CompletedCheckbox : Checkbox}
-              alt={completed ? "Completed Checkbox" : "Checkbox"}
-            />
-          </Box>
-          <Link href={`/tasks/${id}`}>
-            <Text
-              as={completed ? "strike" : undefined}
-              color={color}
-              fontSize={14}
-            >
-              {title}
+    <>
+      <Stack>
+        <Flex justify={"space-between"} mb={2}>
+          <Flex gap={4}>
+            <Text color={"primary.400"} fontSize={14} fontWeight={700}>
+              Tasks
             </Text>
-          </Link>
-          <Box
-            minW={"14px"}
-            cursor={"pointer"}
-            ml={"auto"}
-            onClick={() => destroyTask(id)}
-          >
-            <Image src={Trash} alt={"Trash Can"} />
-          </Box>
+            <Badge borderRadius={"full"} py={"2px"} px={2}>
+              {tasks?.length}
+            </Badge>
+          </Flex>
+          <Flex gap={4}>
+            <Text color={"secondary.400"} fontSize={14} fontWeight={700}>
+              Completed
+            </Text>
+            <Badge
+              borderRadius={"full"}
+              py={"2px"}
+              px={2}
+              style={{ textTransform: "lowercase" }}
+            >
+              {tasks?.filter((task) => task?.completed)?.length} of{" "}
+              {tasks?.length}
+            </Badge>
+          </Flex>
         </Flex>
-      ))}
-    </Stack>
+        {isFetchingTasks ? (
+          <Progress size="xs" isIndeterminate />
+        ) : (
+          <Box h={"4px"} />
+        )}
+        {tasks?.map(({ id, title, color, completed }) => (
+          <Flex
+            key={id}
+            align={"center"}
+            gap={4}
+            bg={"gray.800"}
+            w={"100%"}
+            borderWidth={"1px"}
+            borderColor={"gray.700"}
+            borderRadius={8}
+            p={4}
+          >
+            <Box
+              minW={"18px"}
+              cursor={"pointer"}
+              onClick={() => toggleTask(id, !completed)}
+            >
+              <Image
+                src={completed ? CompletedCheckbox : Checkbox}
+                alt={completed ? "Completed Checkbox" : "Checkbox"}
+              />
+            </Box>
+            <Link href={`/tasks/${id}`}>
+              <Text
+                as={completed ? "strike" : undefined}
+                color={color}
+                fontSize={14}
+              >
+                {title}
+              </Text>
+            </Link>
+            <Box
+              minW={"14px"}
+              cursor={"pointer"}
+              ml={"auto"}
+              onClick={() => {
+                setActiveTaskId(id);
+                setIsConfirming(true);
+              }}
+            >
+              <Image src={Trash} alt={"Trash Can"} />
+            </Box>
+          </Flex>
+        ))}
+      </Stack>
+      <AlertDialog
+        isOpen={isConfirming}
+        leastDestructiveRef={cancelRef}
+        onClose={() => setIsConfirming(false)}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader>Delete Task</AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure? You can&apos;t undo this action afterwards.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={() => setIsConfirming(false)}>
+                Cancel
+              </Button>
+              <Button colorScheme={"red"} onClick={destroyTask} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
